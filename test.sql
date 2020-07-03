4eeb3d0a235ac30bc587df5d8225879040466c78
kenick <zhanggw91@163.com>
2020-07-03 15:00:17 +0800

    数据库文件

--- test.sql
+++ test.sql
@@ -16,6 +16,61 @@ CREATE DATABASE /*!32312 IF NOT EXISTS*/`test` /*!40100 DEFAULT CHARACTER SET la
 
 USE `test`;
 
+/*Table structure for table `account` */
+
+DROP TABLE IF EXISTS `account`;
+
+CREATE TABLE `account` (
+  `id` bigint(11) NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `user_id` bigint(11) DEFAULT NULL COMMENT '用户id',
+  `total` decimal(10,0) DEFAULT NULL COMMENT '总额度',
+  `used` decimal(10,0) DEFAULT NULL COMMENT '已用余额',
+  `residue` decimal(10,0) DEFAULT '0' COMMENT '剩余可用额度',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `branch_table` */
+
+DROP TABLE IF EXISTS `branch_table`;
+
+CREATE TABLE `branch_table` (
+  `branch_id` bigint(20) NOT NULL,
+  `xid` varchar(128) NOT NULL,
+  `transaction_id` bigint(20) DEFAULT NULL,
+  `resource_group_id` varchar(32) DEFAULT NULL,
+  `resource_id` varchar(256) DEFAULT NULL,
+  `lock_key` varchar(128) DEFAULT NULL,
+  `branch_type` varchar(8) DEFAULT NULL,
+  `status` tinyint(4) DEFAULT NULL,
+  `client_id` varchar(64) DEFAULT NULL,
+  `application_data` varchar(2000) DEFAULT NULL,
+  `gmt_create` datetime DEFAULT NULL,
+  `gmt_modified` datetime DEFAULT NULL,
+  PRIMARY KEY (`branch_id`),
+  KEY `idx_xid` (`xid`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Table structure for table `global_table` */
+
+DROP TABLE IF EXISTS `global_table`;
+
+CREATE TABLE `global_table` (
+  `xid` varchar(128) NOT NULL,
+  `transaction_id` bigint(20) DEFAULT NULL,
+  `status` tinyint(4) NOT NULL,
+  `application_id` varchar(32) DEFAULT NULL,
+  `transaction_service_group` varchar(32) DEFAULT NULL,
+  `transaction_name` varchar(64) DEFAULT NULL,
+  `timeout` int(11) DEFAULT NULL,
+  `begin_time` bigint(20) DEFAULT NULL,
+  `application_data` varchar(2000) DEFAULT NULL,
+  `gmt_create` datetime DEFAULT NULL,
+  `gmt_modified` datetime DEFAULT NULL,
+  PRIMARY KEY (`xid`),
+  KEY `idx_gmt_modified_status` (`gmt_modified`,`status`),
+  KEY `idx_transaction_id` (`transaction_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 /*Table structure for table `hibernate_sequence` */
 
 DROP TABLE IF EXISTS `hibernate_sequence`;
@@ -24,9 +79,22 @@ CREATE TABLE `hibernate_sequence` (
   `next_val` bigint(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-/*Data for the table `hibernate_sequence` */
+/*Table structure for table `lock_table` */
 
-insert  into `hibernate_sequence`(`next_val`) values (1);
+DROP TABLE IF EXISTS `lock_table`;
+
+CREATE TABLE `lock_table` (
+  `row_key` varchar(128) NOT NULL,
+  `xid` varchar(96) DEFAULT NULL,
+  `transaction_id` mediumtext,
+  `branch_id` mediumtext,
+  `resource_id` varchar(256) DEFAULT NULL,
+  `table_name` varchar(32) DEFAULT NULL,
+  `pk` varchar(32) DEFAULT NULL,
+  `gmt_create` datetime DEFAULT NULL,
+  `gmt_modified` datetime DEFAULT NULL,
+  PRIMARY KEY (`row_key`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 /*Table structure for table `message_tx` */
 
@@ -49,7 +117,32 @@ CREATE TABLE `message_tx` (
   PRIMARY KEY (`message_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-/*Data for the table `message_tx` */
+/*Table structure for table `order` */
+
+DROP TABLE IF EXISTS `order`;
+
+CREATE TABLE `order` (
+  `id` bigint(11) NOT NULL AUTO_INCREMENT,
+  `user_id` bigint(11) DEFAULT NULL COMMENT '用户id',
+  `product_id` bigint(11) DEFAULT NULL COMMENT '产品id',
+  `count` int(11) DEFAULT NULL COMMENT '数量',
+  `money` decimal(11,0) DEFAULT NULL COMMENT '金额',
+  `status` int(1) DEFAULT NULL COMMENT '订单状态：0：创建中；1：已完结',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `storage` */
+
+DROP TABLE IF EXISTS `storage`;
+
+CREATE TABLE `storage` (
+  `id` bigint(11) NOT NULL AUTO_INCREMENT,
+  `product_id` bigint(11) DEFAULT NULL COMMENT '产品id',
+  `total` int(11) DEFAULT NULL COMMENT '总库存',
+  `used` int(11) DEFAULT NULL COMMENT '已用库存',
+  `residue` int(11) DEFAULT NULL COMMENT '剩余库存',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 /*Table structure for table `t_logger` */
 
@@ -65,8 +158,6 @@ CREATE TABLE `t_logger` (
   `app_name` varchar(128) NOT NULL,
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=latin1;
-
-/*Data for the table `t_logger` */
 
 /*Table structure for table `t_tx_exception` */
 
@@ -85,8 +176,6 @@ CREATE TABLE `t_tx_exception` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-/*Data for the table `t_tx_exception` */
-
 /*Table structure for table `tb_description` */
 
 DROP TABLE IF EXISTS `tb_description`;
@@ -97,8 +186,6 @@ CREATE TABLE `tb_description` (
   `description` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
-
-/*Data for the table `tb_description` */
 
 /*Table structure for table `tb_user` */
 
@@ -112,8 +199,6 @@ CREATE TABLE `tb_user` (
   `description_id` int(11) DEFAULT NULL COMMENT '关联详情id',
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT COMMENT='用户信息表';
-
-/*Data for the table `tb_user` */
 
 /*Table structure for table `transaction_message` */
 
@@ -135,7 +220,23 @@ CREATE TABLE `transaction_message` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-/*Data for the table `transaction_message` */
+/*Table structure for table `undo_log` */
+
+DROP TABLE IF EXISTS `undo_log`;
+
+CREATE TABLE `undo_log` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `branch_id` bigint(20) NOT NULL,
+  `xid` varchar(100) NOT NULL,
+  `context` varchar(128) NOT NULL,
+  `rollback_info` longblob NOT NULL,
+  `log_status` int(11) NOT NULL,
+  `log_created` datetime NOT NULL,
+  `log_modified` datetime NOT NULL,
+  `ext` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `ux_undo_log` (`xid`,`branch_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=53 DEFAULT CHARSET=utf8;
 
 /*Table structure for table `user` */
 
@@ -147,8 +248,6 @@ CREATE TABLE `user` (
   `age` int(11) DEFAULT NULL,
   PRIMARY KEY (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-/*Data for the table `user` */
 
 /* Function  structure for function  `функцийка` */
 
